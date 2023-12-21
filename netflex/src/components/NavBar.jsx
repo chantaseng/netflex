3fd887d655b8b6eb26d82602dd2d9ada9f556bb8
William Chan <chan.william.ts@gmail.com>
2023-12-21 18:58:37 -0500

    creating new component, Navbar and a new page Home. Setting the Routes with BrowserRouter

---
+++ netflex/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <div className="absolute z-[100] flex w-full items-center justify-between p-4">
+      <h1 className="cursor-pointer text-4xl font-bold text-red-600">
+        NETFLEX
+      </h1>
+      <div>
+        <button className="pr-4 text-white">Sign In</button>
+        <button className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
