968212ac883e11a585e2386a360b08966535c18a
William Chan <chan.william.ts@gmail.com>
2023-12-23 12:13:02 -0500

    creating Movie component to make code more readable

--- netflex/src/components/Movie.jsx
+++ netflex/src/components/Movie.jsx
@@ -1,31 +1,31 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-function Movie() {
-  const [movies, setMovies] = useState([]);
-
-  function getMovie() {
-    fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=11e586945712cd25b4a70754556d9313",
-    )
-      .then((res) => res.json())
-      .then((data) => setMovies(data.results));
-  }
-
-  useEffect(() => {
-    getMovie();
-  }, []);
-
-  console.log(movies);
+function Movie({ movie, id }) {
+  const [favorite, setFavorite] = useState(false);
 
   return (
-    <div className="grid grid-cols-6">
-      {movies.map((movie) => (
-        <img
-          className="w-60"
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          key={movie.id}
-        />
-      ))}
+    <div
+      className="relative inline-block w-[160px] cursor-pointer p-2 sm:w-[200px] md:w-[240px] lg:w-[280px]"
+      key={movie?.id}
+    >
+      <img
+        className="block h-auto w-full"
+        src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
+        alt={movie?.title}
+      />
+      <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
+        <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
+          {movie?.title}
+        </p>
+        <p>
+          {favorite ? (
+            <FaHeart className="absolute left-4 top-4 text-gray-400" />
+          ) : (
+            <FaRegHeart className="absolute left-4 top-4 text-gray-400" />
+          )}
+        </p>
+      </div>
     </div>
   );
 }
--- netflex/src/components/Row.jsx
+++ netflex/src/components/Row.jsx
@@ -1,10 +1,9 @@
 import axios from "axios";
-import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import Movie from "./Movie";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
 
   useEffect(
     function () {
@@ -14,7 +13,7 @@ function Row({ title, fetchURL }) {
     },
     [fetchURL],
   );
-  console.log(movies);
+  // console.log(movies);
 
   return (
     <>
@@ -22,28 +21,7 @@ function Row({ title, fetchURL }) {
       <div className="relative flex items-center">
         <div className={"slider"}>
           {movies.map((movie, id) => (
-            <div
-              className="relative inline-block w-[160px] cursor-pointer p-2 sm:w-[200px] md:w-[240px] lg:w-[280px]"
-              key={movie?.id}
-            >
-              <img
-                className="block h-auto w-full"
-                src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-                alt={movie?.title}
-              />
-              <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
-                <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
-                  {movie?.title}
-                </p>
-                <p>
-                  {like ? (
-                    <FaHeart className="absolute left-4 top-4 text-gray-400" />
-                  ) : (
-                    <FaRegHeart className="absolute left-4 top-4 text-gray-400" />
-                  )}
-                </p>
-              </div>
-            </div>
+            <Movie movie={movie} key={id} />
           ))}
         </div>
       </div>
