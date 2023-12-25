cbb32cf100b83059de5d8432d61a64d0320cb6bd
William Chan <chan.william.ts@gmail.com>
2023-12-24 23:00:47 -0500

    login, signup and account page created. Adding routes for the new pages and Link for the logo and the signin and signup button

--- netflex/src/App.jsx
+++ netflex/src/App.jsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
 
 import Navbar from './components/NavBar';
 import Home from './pages/Home';
-import { AuthContextProvider } from './context/AuthContext';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Account from './pages/Account';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
--- netflex/src/components/NavBar.jsx
+++ netflex/src/components/NavBar.jsx
@@ -1,14 +1,22 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <div className="absolute z-[100] flex w-full items-center justify-between p-4">
-      <h1 className="cursor-pointer text-4xl font-bold text-red-600">
-        NETFLEX
-      </h1>
+      <Link to="/">
+        <h1 className="cursor-pointer text-4xl font-bold text-red-600">
+          NETFLEX
+        </h1>
+      </Link>
       <div>
-        <button className="pr-4 text-white">Sign In</button>
-        <button className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white">
-          Sign Up
-        </button>
+        <Link to="/login">
+          <button className="pr-4 text-white">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          <button className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white">
+            Sign Up
+          </button>
+        </Link>
       </div>
     </div>
   );
