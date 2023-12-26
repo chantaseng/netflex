cd15953fe1444e7d11e5f0d2d8153b89398d282d
William Chan <chan.william.ts@gmail.com>
2023-12-26 17:13:36 -0500

    navbar, ternary op, navbar has different option when a user a log in. Creating a handle submit function when loggin out

--- netflex/src/components/NavBar.jsx
+++ netflex/src/components/NavBar.jsx
@@ -1,6 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
 
 function Navbar() {
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  // console.log(user.email);
+
+  const handleLogout = async function () {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="absolute z-[100] flex w-full items-center justify-between p-4">
       <Link to="/">
@@ -8,16 +22,30 @@ function Navbar() {
           NETFLEX
         </h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="pr-4 text-white">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white">
-            Sign Up
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="pr-4 text-white">Account</button>
+          </Link>
+          <button
+            onClick={handleLogout}
+            className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white"
+          >
+            Log Out
           </button>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="pr-4 text-white">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="cursor-pointer rounded bg-red-600 px-6 py-2 text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
