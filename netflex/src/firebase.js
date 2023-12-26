0279543f6fc16967db6244e555cbb7d5bd6acf2b
William Chan <chan.william.ts@gmail.com>
2023-12-26 18:04:29 -0500

    connecting app with firestore

--- netflex/src/firebase.js
+++ netflex/src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,5 +20,6 @@ console.log('Firebase Config:', firebaseConfig);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
