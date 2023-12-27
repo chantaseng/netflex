a547a257be7dcdd22649492af6afe78a579bc32c
William Chan <chan.william.ts@gmail.com>
2023-12-26 20:35:54 -0500

    deleting movie from account component

--- netflex/src/components/SavedMovies.jsx
+++ netflex/src/components/SavedMovies.jsx
@@ -1,4 +1,5 @@
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { AiOutlineClose } from 'react-icons/ai';
 import { UserAuth } from '../context/AuthContext';
 import { db } from '../firebase';
 import { updateDoc, doc, onSnapshot } from 'firebase/firestore';
@@ -27,6 +28,18 @@ function SavedMovies() {
     [user?.email],
   );
 
+  const movieRef = doc(db, 'users', `${user?.email}`);
+  const deleteMovie = async function (passedID) {
+    try {
+      const result = movies.filter((movie) => movie?.id !== passedID);
+      await updateDoc(movieRef, {
+        savedMovies: result,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h2 className="p-4 font-bold text-white md:text-xl">My Movies</h2>
@@ -53,6 +66,12 @@ function SavedMovies() {
               <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
                 <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
                   {movie?.title}
+                </p>
+                <p
+                  onClick={() => deleteMovie(movie.id)}
+                  className="absolute right-4 top-4 text-gray-300"
+                >
+                  <AiOutlineClose />
                 </p>
               </div>
             </div>
