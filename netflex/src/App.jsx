8a032d2988ddec0a90a8cfefca128c28378284d0
William Chan <chan.william.ts@gmail.com>
2024-01-10 18:26:20 -0500

    moved Search file to pages folder

--- netflex/src/App.jsx
+++ netflex/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Account from './pages/Account';
-import Search from './components/Search';
+import Search from './pages/Search';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
