f6da50b06ba72935aa737cdfd593ee12b18e80b0
William Chan <chan.william.ts@gmail.com>
2024-01-21 03:25:33 -0500

    Building MovieInfo component, so far, poster, title of movie have been added to the page

--- netflex/src/components/Movie.jsx
+++ netflex/src/components/Movie.jsx
@@ -10,7 +10,6 @@ import { NavLink, useNavigate } from 'react-router-dom';
 function Movie({ movie }) {
   const [favorite, setFavorite] = useState(false);
   const [saved, setSaved] = useState(false);
-  const [movieInfo, setMovieInfo] = useState([]);
   const navigate = useNavigate();
   const { user } = UserAuth();
 
@@ -34,57 +33,41 @@ function Movie({ movie }) {
     }
   };
 
-  const key = import.meta.env.VITE_REACT_APP_API_KEY;
-
   const id = movie.id;
 
-  // const fetchData = async function () {
-  //   try {
-  //     await axios
-  //       .get(`https://api.themoviedb.org/3/movie/${id}?language=en-US${key}`)
-  //       .then((res) => setMovieInfo(res.data));
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
-  // };
-  // console.log(movieInfo);
-
   const handleMovieInfo = function () {
-    // fetchData();
-    navigate(`/movie/${id}`, { state: { movieInfo } });
-
-    // return <MovieInfo movieInfo={movieInfo} />;
+    navigate(`/movie/${id}`);
   };
 
   return (
     <>
-      <NavLink to={`/movie/${id}`}>
-        {movie.backdrop_path === null ? null : (
-          <div
-            className="relative inline-block w-[160px] cursor-pointer p-2 sm:w-[200px] md:w-[240px] lg:w-[280px]"
-            key={movie?.id}
-            onClick={handleMovieInfo}
-          >
-            <img
-              className="block h-auto w-full"
-              src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
-              alt={movie?.title}
-            />
-            <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
-              <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
-                {movie?.title}
-              </p>
-              <p onClick={saveToWatchList}>
-                {favorite ? (
-                  <FaHeart className="absolute right-4 top-4 text-gray-400" />
-                ) : (
-                  <FaRegHeart className="absolute right-4 top-4 text-gray-400" />
-                )}
-              </p>
-            </div>
+      {/* <NavLink to={`/movie/${id}`}> */}
+      {movie.backdrop_path === null ? null : (
+        <div
+          className="relative inline-block w-[160px] cursor-pointer p-2 sm:w-[200px] md:w-[240px] lg:w-[280px]"
+          key={movie?.id}
+          onClick={handleMovieInfo}
+        >
+          <img
+            className="block h-auto w-full"
+            src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+          <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
+            <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
+              {movie?.title}
+            </p>
+            <p onClick={saveToWatchList}>
+              {favorite ? (
+                <FaHeart className="absolute right-4 top-4 text-gray-400" />
+              ) : (
+                <FaRegHeart className="absolute right-4 top-4 text-gray-400" />
+              )}
+            </p>
           </div>
-        )}
-      </NavLink>
+        </div>
+      )}
+      {/* </NavLink> */}
     </>
   );
 }
--- netflex/src/components/MovieInfo.jsx
+++ netflex/src/components/MovieInfo.jsx
@@ -1,17 +1,91 @@
-import { useLocation } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function MovieInfo() {
-  const location = useLocation();
-  const movieInfo = location.state?.movieInfo;
+  const [movie, setMovie] = useState([]);
+  const [dataFetched, setDataFetched] = useState(false);
+  let { id } = useParams();
 
-  if (!movieInfo) {
-    // Handle the case where movieInfo is not available
-    return <div>Loading...</div>;
-  }
+  const key = import.meta.env.VITE_REACT_APP_API_KEY;
 
-  console.log(movieInfo);
+  // useEffect will only run once because of the state dataFetched
+  useEffect(() => {
+    const fetchData = async function () {
+      try {
+        if (!dataFetched) {
+          await axios
+            .get(
+              `https://api.themoviedb.org/3/movie/${id}?language=en-US${key}`,
+            )
+            .then((res) => setMovie(res.data));
+          setDataFetched(true);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
 
-  return <div className="text-white">hello</div>;
+    fetchData();
+  }, [id, dataFetched]);
+  console.log(movie);
+
+  const dateString = movie?.release_date;
+  const year = dateString ? parseInt(dateString.split('-')[0]) : null;
+  // console.log(year);
+
+  return (
+    <>
+      <div className="h-[550px] w-full">
+        <div className="h-full w-full">
+          <div className="absolute h-[550px] w-full bg-slate-400 bg-opacity-80"></div>
+          <img
+            className="h-full w-full object-cover"
+            src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+            alt={movie?.title}
+          />
+          <div className="absolute top-[0%] my-10 h-[490px] w-full p-4 md:p-8">
+            <div className="flex">
+              <div>
+                <img
+                  className="h-[440px] w-[400px] rounded-md p-4"
+                  src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+                  alt={movie?.title}
+                />
+              </div>
+
+              <div className="flex flex-col text-white">
+                <div className="mt-10 flex w-full pl-10">
+                  <h1 className="font-bold md:text-3xl lg:text-5xl">
+                    {movie?.title}{' '}
+                    <span className="pl-1 font-thin md:text-3xl lg:text-5xl">
+                      ({year})
+                    </span>
+                  </h1>
+                </div>
+
+                <p className="pl-10 pt-2">
+                  <div className="flex">
+                    <span>{movie?.release_date}</span>
+                    <span className="flex">
+                      {movie?.genres &&
+                        movie?.genres.map((genre, id) => (
+                          <div key={id}>
+                            <p>{genre.name}</p>
+                          </div>
+                        ))}
+                    </span>
+                    <span>{movie?.runtime} minutes</span>
+                  </div>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-ful h-96 bg-white">actor face here</div>
+      </div>
+    </>
+  );
 }
 
 export default MovieInfo;
