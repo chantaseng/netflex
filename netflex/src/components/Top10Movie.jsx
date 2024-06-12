8ce5777af183fee9679f90626c8df60f26de7992
William Chan <chan.william.ts@gmail.com>
2024-06-11 19:56:25 -0600

    fixing favorite position on top10movie cards

--- netflex/src/components/Top10Movie.jsx
+++ netflex/src/components/Top10Movie.jsx
@@ -61,15 +61,15 @@ const Top10Movie = ({ movie, index }) => {
               src={`https://image.tmdb.org/t/p/w500${movie?.backdrop_path}`}
               alt={movie?.title}
             />
-            <div className="absolute left-0 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
+            <div className="absolute left-10 top-0 h-full w-full text-white  opacity-0 hover:bg-black/80 hover:opacity-100">
               <p className="flex h-full items-center justify-center whitespace-normal text-center text-xs font-bold md:text-sm">
                 {movie?.title}
               </p>
               <p onClick={saveToWatchList}>
                 {favorite ? (
-                  <FaHeart className="heart-icon absolute right-4 top-4 text-gray-400" />
+                  <FaHeart className="heart-icon absolute right-[68px] top-7 text-gray-400" />
                 ) : (
-                  <FaRegHeart className="heart-icon absolute right-4 top-4 text-gray-400" />
+                  <FaRegHeart className="heart-icon absolute right-[68px] top-7 text-gray-400" />
                 )}
               </p>
             </div>
